cce69c0ceb5aa3088d08a40f7eaa4db2d36cb283
mustafa <msolmaz83@gmail.com>
2023-10-22 23:04:13 +0200

    sidebar

--- src/pages/layouts/main/sidebar/menu/index.jsx
+++ src/pages/layouts/main/sidebar/menu/index.jsx
@@ -1,13 +1,14 @@
 import { NavLink } from "react-router-dom"
 import classNames from "classnames"
 import { mainMenu } from "../../../../../utils/consts"
-import Button from "../../../../../components/button"
+
+import More from "./more"
+import New from "./new"
 
 
 const Menu = () => {
   return (
     <nav className="mt-0.5 mb-1">
-
       {
         mainMenu.map((menu,index)=>(
   <NavLink to={menu.path} className="py-1 block group">
@@ -16,40 +17,19 @@ const Menu = () => {
               <div className="w-[18px] h-[18px] relative"> {
                 menu?.notification && ( <span className="w-[18px] h-[18px] rounded-full bg-[#1d9bf0] absolute -top-1.5 -right-1 flex item-center justify-center text-[11px]" >{menu?.notification} </span>)
               }
-             
               {!isActive && menu.icon.passive}
-
              {isActive &&  menu.icon.active}  
               </div>
-            
             <div className="pr-4 text-xl">
             {menu.title}
             </div>
           </div>
         )}
       </NavLink>
-
         ))
       }
-      <button className="py-1 block group">
-     
-          <div className="p-3 rounded-full transition-colors inline-flex pr-4 items-center gap-5 text-xl group-hover:bg-[#eff3f41a]">
-              <div className="w-[18px] h-[18px] relative"> 
-              <svg viewBox="0 0 24 24" aria-hidden="true" width={26.25} height={26.25} fill="#fff"><g><path d="M3.75 12c0-4.56 3.69-8.25 8.25-8.25s8.25 3.69 8.25 8.25-3.69 8.25-8.25 8.25S3.75 16.56 3.75 12zM12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-4.75 11.5c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25S6 11.31 6 12s.56 1.25 1.25 1.25zm9.5 0c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25zM13.25 12c0 .69-.56 1.25-1.25 1.25s-1.25-.56-1.25-1.25.56-1.25 1.25-1.25 1.25.56 1.25 1.25z"></path></g></svg>
-            
-              </div>
-            
-            <div className="pr-4 text-xl">
-         More
-            </div>
-          </div>
-     
-      </button>
-      <div className="py-4 w-[90%]">
-         <Button size='large'>Post</Button>
-      </div>
-     
-     
+    <More/>
+    <New/>
     </nav>
   )
 }
---
+++ src/pages/layouts/main/sidebar/menu/new/index.jsx
@@ -0,0 +1,12 @@
+import Button from "../../../../../../components/button"
+
+
+const New = () => {
+  return (
+    <div className="py-4 w-[90%]">
+    <Button size='large'>Post</Button>
+ </div>
+  )
+}
+
+export default New
