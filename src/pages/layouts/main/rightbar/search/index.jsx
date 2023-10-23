8ece956aeffd7ecd39bc7eb5f6b77e2662e76fef
mustafa <msolmaz83@gmail.com>
2023-10-23 23:23:41 +0200

    search

--- src/pages/layouts/main/rightbar/search/index.jsx
+++ src/pages/layouts/main/rightbar/search/index.jsx
@@ -9,11 +9,12 @@ const Search = () => {
     const ref = useRef()
     useClickAway(ref,()=>{
         console.log('outside cliked')
+        setFocus(false)
     })
 
 
   return (
-    <div className="min-h-[32px] h-[53px] mb-3 flex items-center relative">
+    <div ref ={ref} className="min-h-[32px] h-[53px] mb-3 flex items-center relative">
         <label className="h-[43px] rounded-full bg-[#202327] w-full relative group border border-transparent focus-within:bg-black focus-within:border-[#1d9bf0]">
             <div className="w-[56px] h-full flex items-center justify-center absolute top-0 left-0">
             <svg viewBox="0 0 24 24" className="min-w-[32px] text-[#71767b]" width={18.75} ><g><path fill="currentColor" d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path></g></svg>
@@ -26,8 +27,7 @@ const Search = () => {
              onChange={e=>setQuery(e.target.value)}
               className="w-full h-full bg-transparent rounded-full outline-none pl-[56px] text-[15px]"></input>
             
-        {
-           ( query && focus )&& (
+        {( query && focus )&& (
                 <button
                 type="button"
                 onClick={()=>setQuery('')}
@@ -38,7 +38,7 @@ const Search = () => {
             
         </label>
            {focus && (
-            <div className="absolute top-full w-full bg-black shadow-box px-4 max-h-[calc(80vh-53px)] rounded-lg min-h-[100px]">
+            <div className="absolute top-full w-[350px] -left-px -translate-y-p1 bg-black shadow-box px-4 max-h-[calc(80vh-53px)] rounded-lg min-h-[100px]">
                 <p className="p-3 pt-5 text-[#71767b] leading-5" >Try searching for people, lists, or keywords</p>
             </div>
            )}
